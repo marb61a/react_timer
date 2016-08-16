415dd07015feb89957c0949ae9ea61800ca39d81
martin byrne <marb61a@gmail.com>
2016-08-16 10:22:56 +0000

    updated navigation.jsx

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -3,8 +3,29 @@ var {Link, IndexLink} = require("react-router");
 
 var Navigation = () => {
     return(
-        <div>
-        </div>    
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">
+                        React Timer App
+                    </li>
+                    <li>
+                        <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/countdown" activeClassName="active-link">Countdown</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                    <li className="menu-text">
+                        Created by <a href="" target="_blank">Martin</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
     );    
 };
 
