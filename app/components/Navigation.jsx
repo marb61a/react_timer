96c1e931500fd03216fa88a7be32fce3cdd9eac2
martin byrne <marb61a@gmail.com>
2016-08-16 10:01:20 +0000

    added Main.jsx

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,11 @@
+var React = require("react");
+var {Link, IndexLink} = require("react-router");
+
+var Navigation = () => {
+    return(
+        <div>
+        </div>    
+    );    
+};
+
+module.exports = Navigation;
