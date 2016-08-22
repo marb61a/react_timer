eacda9b7259631e32200529d7a7d13b66d748188
martin byrne <marb61a@gmail.com>
2016-08-22 10:20:19 +0000

    updated timer test component

--- app/tests/components/Timer.test.jsx
+++ app/tests/components/Timer.test.jsx
@@ -23,4 +23,32 @@ describe('Timer', () => {
             done();    
         }, 1001);
     });
+    
+    it('should pause timer on paused status', (done) => {
+        var timer = TestUtils.renderIntoDocument(<Timer/>);
+        
+        timer.setState({count : 10 });
+        timer.handleStatusChange('started');
+        timer.handleStatusChange('paused');
+        
+        setTimeout(() => {
+            expect(timer.state.timerStatus).toBe('paused');
+            expect(timer.state.count).toBe(10);
+            done();    
+        }, 1001);
+    });
+    
+    it('should clear timer on stopped status', (done) => {
+        var timer = TestUtils.renderIntoDocument(<Timer/>); 
+        
+        timer.setState({count : 10 });
+        timer.handleStatusChange('started');
+        timer.handleStatusChange('stopped');
+        
+        setTimeout(() => {
+            expect(timer.state.timerStatus).toBe('stopped');
+            expect(timer.state.count).toBe(0);
+            done();    
+        }, 1001);
+    });
 })
